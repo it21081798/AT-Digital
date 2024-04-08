67b094ea2252696624be6c22ede400c644b28ab5
Thamiru <it21081798@my.sliit.lk>
2024-04-08 11:52:07 +0530

    update four

--- at digital/src/components/Home.js
+++ at digital/src/components/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
         </div>
         <div className=' lg:mt-0 mx-auto absolute bottom-0 left-0 lg:left-[60px] lg:top-[354px] lg:w-[630px] w-screen h-auto md:h-[306px] bg-gradient-to-l from-[#1CBDDD] to-[#4DCA79] px-[40px] py-[24px]'>
           <p className='text-[36px] leading-[36px] md:text-[48px] text-white md:leading-[48px] font-primary font-bold'>We crush your competitors, goals, and sales records - without the B.S.</p>
-          <button className='bg-[#F28D35] rounded-[4px] font-bold text-[14px] text-white py-[12px] px-[20px] uppercase mt-4 '>Get free consultation</button>
+          <button className='bg-[#F28D35] rounded-[4px] font-medium text-[14px] text-white py-[12px] px-[20px] uppercase mt-4 '>Get free consultation</button>
         </div>
       </div>
     );
